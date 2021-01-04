ea86e7d0d5e58a8da9541983f6ed4e38ab9a629f
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2021-01-05 00:55:13 +0900

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - homepage-sa',
+    titleTemplate: '%s',
     title: 'Saki Adachi',
     meta: [
       { charset: 'utf-8' },
