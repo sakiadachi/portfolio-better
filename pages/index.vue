8d578e0ea91d23f2cbe484109f1aa67b51422676
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2021-01-05 00:04:59 +0900

    add links

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,47 @@
 <template>
     <div>
         <div class="pt-8 pl-8 pr-8 pb-16">
-            <h1 class="xlarge">Saki Adachi</h1>
-            <h1 class="">Frontend Developer / Designer Based in Tokyo</h1>
+            <div class="profile_link black--text">
+                <h1 class="xlarge">Saki Adachi</h1>
+                <h1 class="">Frontend Developer / Designer Based in Tokyo</h1>
+            </div>
             <!-- <p class="my-6">I code and design things.</p> -->
             <Profile></Profile>
         </div>
 
-        <!-- <Projects></Projects> -->
+        <div class="links">
+            <v-row no-gutters>
+                <v-col>
+                    <a
+                        href="https://codepen.io/sakiadachi88"
+                        target="_blank"
+                        class="white--text hov-blue"
+                        >codepen</a
+                    >
+
+                    <a
+                        href="https://dribbble.com/sakiadachi88"
+                        target="_blank"
+                        class="white--text hov-yellow"
+                        >Qiita</a
+                    >
+
+                    <a
+                        href="https://dribbble.com/sakiadachi88"
+                        target="_blank"
+                        class="white--text hov-pink"
+                        >dribbble</a
+                    >
+
+                    <a
+                        href="https://coconala.com/users/1790932"
+                        target="_blank"
+                        class="white--text hov-orange"
+                        >coconala</a
+                    >
+                </v-col>
+            </v-row>
+        </div>
     </div>
 </template>
 
