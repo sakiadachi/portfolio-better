0684f75361645f9f65efd6809b12d650bb43799e
sakiadachi <sakiadachi.88@gmail.com>
2023-06-26 23:56:32 +0900

    branch: change-to-vue-3 - TodoList adjust to pinia, vue3

--- src/store/todoList.ts
+++ src/store/todoList.ts
@@ -1,60 +1,59 @@
 // Utilities
 import { defineStore } from 'pinia'
 
-type TodoItemType = {
-  title: string,
-  done: boolean,
+export type TodoItemType = {
+  title: string
+  done: boolean
 }
 
 interface State {
   list: string[]
-  newItem: string
+  newItemText: string
   newList: []
 }
 
 function syncLocalStorage(content: TodoItemType[]) {
   localStorage.setItem('todolist', JSON.stringify(content))
-},
+}
 
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    list: localStorage.getItem('todolist') || [],
-    newItem: '',
+    list: localStorage.getItem('todolist')
+      ? JSON.parse(localStorage.getItem('todolist') || `[]`)
+      : [],
+    newItemText: '',
     newList: [],
+    toDoRules: [(v: string) => !!v || 'Cannot save empty todo'],
   }),
-  getters: {
-    
-  },
   actions: {
-    
-    add(state:State, text:string) {
-      state.list.push({
-          title: text,
-          done: false,
+    add(text: string) {
+      this.$state.list.push({
+        title: text,
+        done: false,
       })
-      syncLocalStorage(state.list)
+      syncLocalStorage(this.$state.list)
     },
-    removeItem(index:number) {
-        this.$state.list.splice(todo, 1)
-        syncLocalStorage(state.list)
+    removeItem(index: number) {
+      this.$state.list.splice(index, 1)
+      syncLocalStorage(this.$state.list)
     },
-    removeDone(state) {
-        const result = state.list.filter((x) => {
-            return x.done === false
-        })
-        state.list = result
-        syncLocalStorage(state.list)
+    removeDone() {
+      const result = this.$state.list.filter((x: TodoItemType) => {
+        return x.done === false
+      })
+      this.$state.list = result
+      syncLocalStorage(this.$state.list)
     },
-    removeAll(state) {
-        state.list = []
-        syncLocalStorage(state.list)
+    removeAll() {
+      this.$state.list = []
+      syncLocalStorage(this.$state.list)
     },
-    toggle(state, todo) {
-        todo.done = !todo.done
-        syncLocalStorage(state.list)
+    toggle(todo: TodoItemType) {
+      todo.done = !todo.done
+      syncLocalStorage(this.$state.list)
     },
-    updateTodoList(state) {
-        syncLocalStorage(state.list)
+    updateTodoList() {
+      syncLocalStorage(this.$state.list)
     },
   },
 })
