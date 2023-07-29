d73547d50a342e53913ab1e093f7261548917f51
sakiadachi <sakiadachi.88@gmail.com>
2023-07-29 18:21:26 +0900

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
     <v-main>
       <router-view />
     </v-main>
+    <common-footer />
   </v-app>
 </template>
 
 <script lang="ts" setup>
 import AppBar from './components/common/AppBar.vue'
+import CommonFooter from './components/common/CommonFooter.vue'
 </script>
---
+++ src/components/common/CommonFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer class="pa-10">
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Saki Adachi</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+<script lang="ts" setup>
+//
+</script>
